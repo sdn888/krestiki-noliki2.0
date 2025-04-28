c6a8adefdda5286ec23cc3b0cd174fa5f8b27dff
sdn888 <sdn899@gmail.com>
2025-04-28 12:36:36 +0300

    new logic code had been added

--- main.py
+++ main.py
@@ -9,7 +9,25 @@ current_player = "X"
 buttons = []
 
 def on_click(row, col):
-    pass
+    global current_player
+    buttons[row][col]['text'] = current_player
+    if check_winner():
+        messagebox.showinfo("Игра окончена",f"Игрок {current_player} победил!")
+    current_player = "0" if current_player == "X" else "X"
+
+def check_winner():
+    for i in range(3):
+        if buttons[i][0]["text"] == buttons[i][1]["text"] == buttons[i][2]["text"] != "":
+            return True
+        if buttons[0][i]["text"] == buttons[1][i]["text"] == buttons[2][i]["text"] != "":
+            return True
+        if buttons[0][0]["text"] == buttons[1][1]["text"] == buttons[2][2]["text"] != "":
+            return True
+        if buttons[0][2]["text"] == buttons[1][1]["text"] == buttons[2][0]["text"] != "":
+            return True
+    return False
+
+
 
 for i in range(3):
     row = []
